65fab3168f5bbfe529048f83dde52d828df09c51
Pablo Manuel Fernández Velazquez <163730673+ProfitCoder@users.noreply.github.com>
2025-11-27 12:27:00 +0100

    Add files via upload

---
+++ Unidad 1 MySQL/000.- (AGGREGATE FUNCTIONS) Funciones de añadido.sql
@@ -0,0 +1,15 @@
+-- Funciones de AGGREGATE
+
+-- Count (Cuenta el nº de filas)
+select count * from mascotas;
+
+-- Max (Devuelve el maximo de una determinada columna)
+select max altura from clientes;
+
+-- Min (Devuelve el min)
+select min altura  from clientes;
+
+-- Sum (Devuelve la suma)
+select sum * from clientes;
+
+-- AVG (Devuelve la media)
